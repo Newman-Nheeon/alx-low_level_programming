0ddd49ebdf8239aa0491cff9b33833e52c2fd901
Newman <newmanenyoojo@gmail.comgit config --global user.email newmanenyoojo@gmail.com>
2022-04-09 09:50:12 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-  *main - entry point
-  *Return: 0 after printing the Function
-  */
+ *main - entry point
+ *Return: 0 after printing the Function
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle")
