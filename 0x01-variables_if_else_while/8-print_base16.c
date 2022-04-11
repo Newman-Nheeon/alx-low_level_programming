1dcebe890b31446650cdb94736d6a4371fd819d0
Newman-Nheeon <newmanenyoojo@gmail.com>
2022-04-11 16:39:50 +0100

    PUTCHAR/For

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 16; i++)
+	{
+		putchar (i);
+	}
+	putchar('\n');
+	return (0);
