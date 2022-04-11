17f0eb3a5159c3fd2c26d2333d8dd455cb789f7f
Newman-Nheeon <newmanenyoojo@gmail.com>
2022-04-11 16:44:01 +0100

    PUTCHAR/For

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 16; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		putchar (i);
+	}
+	for (i = 97; i <= 102; i++)
+	{
+		putchar(i)
 	}
 	putchar('\n');
 	return (0);
