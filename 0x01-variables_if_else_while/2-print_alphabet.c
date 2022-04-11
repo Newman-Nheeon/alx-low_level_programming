dc54457fd3753926f9f563527f5620e39ec8f882
Newman-Nheeon <newmanenyoojo@gmail.com>
2022-04-11 13:33:11 +0100

    Putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * Main - Entry Point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char alphabeth;
+
+	for (alphabeth = 'a'; alphabeth <= 'z'; alphabeth++)
+	{
+		putchar(alphabeth);
+	}
+	putchar(\n);
+
+	return;
+		
+}
