69e126bc4123626ddc70236687e3a38a47606aff
Newman-Nheeon <newmanenyoojo@gmail.com>
2022-04-11 14:54:23 +0100

    PRINT/For

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int base10;
+
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	{
+		prinf(%base10, base10);
+	}
+	printf('\n');
+	return (0);
+}
+
