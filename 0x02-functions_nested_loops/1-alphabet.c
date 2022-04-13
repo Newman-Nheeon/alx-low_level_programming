f28d0002955071254d33ce93527f83de4c035ac2
Newman-Nheeon <newmanenyoojo@gmail.com>
2022-04-13 14:05:07 +0100

    Printf Putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++;)
+	{
+		putchar(i);
+	}
+	putchar('\n')
+		return (0);
+}
