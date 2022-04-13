70f78dc34cb592fd7c7ad4a0e00a12ea8544662b
Newman-Nheeon <newmanenyoojo@gmail.com>
2022-04-13 15:38:13 +0100

    Last Digit Value

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit
+ * @n: takes in an integer
+ * Return: 0 (Success)
+ */
+int print_last_digit(int)
+{
+	int ln = n % 10;
+	if (n < 0)
+		ln = ln * -1;
+	_putchar(ln + '0');
+	return (ln);
