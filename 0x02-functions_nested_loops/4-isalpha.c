256d4b7db4f0750d1b8a7a73cc46dd49bf90d582
Newman-Nheeon <newmanenyoojo@gmail.com>
2022-04-13 14:46:07 +0100

    is Lower&alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isalpha - entry point
+ * @c
+ * Return
+ */
+int _isalpha(int c)
+{
+	if (c == 'c' || c == 'C')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
