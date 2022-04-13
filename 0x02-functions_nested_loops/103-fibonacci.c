4645d6802b48b0256e5cd47764de5e840f0190ff
Newman-Nheeon <newmanenyoojo@gmail.com>
2022-04-13 23:03:32 +0100

    Advance 04

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,19 +6,22 @@
  */
 int main(void)
 {
-	unsigned long fib1 = 0, fib2 = 1, fibsum;
-	float tot_sum;
+	unsigned long count, i, j, k, sums;
 
-	while (1)
+	i = sums = 0;
+	j = 1;
+
+
+	for (count = 0; count < 50; count++)
 	{
-		fibsum = fib1 + fib2;
-		if (fibsum > 4000000)
-			break;
-		if ((fibsum % 2) == 0)
-			tot_sum += fibsum;
+		k = i + j;
+		i = j;
+		j = k;
 
-		fib1 = fib2;
-		fib2 = fibsum;
+		if (k % 2 == 0 && k < 4000000)
+		{
+			sums += k;
+		}
 	}
 	printf("%lu\n", tot_sum);
 	return (0);
