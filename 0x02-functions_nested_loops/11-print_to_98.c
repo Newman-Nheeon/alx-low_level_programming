b9d63ed82b3fc43deb692b16bcea816fcf50aa46
Newman-Nheeon <newmanenyoojo@gmail.com>
2022-04-13 16:57:24 +0100

    PRINT OUT NATURAL NUMBERS

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_to_98 - entry point
+ * @n: takes an integer
+ * Return: output
+ */
+void print_to_98(int n)
+{
+	int i;
+	if (n > 98)
+	{
+		for (i = n; i > 97; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}else
+	{
+		for (i = n; i < 99; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	print("\n");
+}
