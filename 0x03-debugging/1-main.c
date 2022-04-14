b28fcbebb8c66acd77d96d0b8083b913a07b891a
Newman-Nheeon <newmanenyoojo@gmail.com>
2022-04-14 13:05:10 +0100

    Debugging-02

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,12 +13,13 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
+
 	/*
 	 * while (i < 10)
 	 * {
 	 * Infinite loop - No increase of variable
 	 * putchar(i);
-	 * }
+	  }
 	 */
 
 	printf("Infinite loop avoided! \\o/\n");
