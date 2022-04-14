68366b1eb820f6489a0aaf6426edd01d218544af
Newman-Nheeon <newmanenyoojo@gmail.com>
2022-04-14 12:36:39 +0100

    Debugging-01

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * main - tests function that prints if integer is positive or negative
+ * main - entry point
+ * 
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
 	i = 0;
 
-	positive_or_negative(i);
-	
+	positive_to_negative(i);
+
 	return (0);
 }
